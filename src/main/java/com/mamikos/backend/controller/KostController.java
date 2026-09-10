f946b5e323d58589c0c6a973d8277018217a3451
silendas <muhammadyasminnulhakim34@gmail.com>
2026-09-10 14:55:03 +0700

    refactor: remove unused Tag import in KostController

--- src/main/java/com/mamikos/backend/controller/KostController.java
+++ src/main/java/com/mamikos/backend/controller/KostController.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.security.Principal;
 import java.util.List;
