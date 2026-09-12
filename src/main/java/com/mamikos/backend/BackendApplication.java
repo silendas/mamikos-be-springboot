feb5dc0b8aba9af1df22a716e1e22c1d8cf2e9ef
silendas <muhammadyasminnulhakim34@gmail.com>
2026-09-12 08:50:27 +0700

    feat: add dotenv-java support to automatically load .env file on application startup

--- src/main/java/com/mamikos/backend/BackendApplication.java
+++ src/main/java/com/mamikos/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.mamikos.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,9 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
+
 
 
